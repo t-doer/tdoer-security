1ef114b3be36dfb5eafcd74ea18409e2cf23df27
Htinker Hu <htinker@163.com>
2019-09-16 23:55:36 +0800

    init commit

---
+++ src/main/java/com/tdoer/security/autoconfigure/ManagementConfiguration.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2017-2019 T-Doer (tdoer.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tdoer.security.autoconfigure;
+
+import com.tdoer.security.crypto.password.MD5PasswordEncoder;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author Htinker Hu (htinker@163.com)
+ * @create 2017-09-19
+ */
+@Configuration
+@Order(-10)
+public class ManagementConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .requestMatchers().antMatchers("/management/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/management/info", "/management/health").permitAll()
+                .anyRequest().hasRole("SYSTEM_OPERATOR")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+        ;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(new MD5PasswordEncoder())
+                .withUser("tdoer")
+                .password("fb3e9deaa6bffee9315c56acab2bd1e7") // md5 of operator@tdoer
+                .roles("SYSTEM_OPERATOR");
+    }
+}
---
+++ src/main/java/com/tdoer/security/crypto/password/MD5PasswordEncoder.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright 2017-2019 T-Doer (tdoer.com).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.tdoer.security.crypto.password;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.security.MessageDigest;
+
+/**
+ * @author Htinker Hu (htinker@163.com)
+ * @create 2017-09-19
+ */
+public class MD5PasswordEncoder implements PasswordEncoder {
+    private final static String[] hexDigits = {"0", "1", "2", "3", "4", "5", "6", "7",
+            "8", "9", "a", "b", "c", "d", "e", "f"};
+
+    @Override
+    public String encode(CharSequence rawPassword) {
+        return encodeMD5(rawPassword.toString());
+    }
+
+    /**
+     * Verify the encoded password obtained from storage matches the submitted raw
+     * password after it too is encoded. Returns true if the passwords match, false if
+     * they do not. The stored password itself is never decoded.
+     *
+     * @param rawPassword     the raw password to encode and match
+     * @param encodedPassword the encoded password from storage to compare with
+     * @return true if the raw password, after encoding, matches the encoded password from
+     * storage
+     */
+    @Override
+    public boolean matches(CharSequence rawPassword, String encodedPassword) {
+        return encodeMD5(rawPassword.toString()).equals(encodedPassword);
+    }
+
+
+    private String byteArrayToHexString(byte[] b) {
+        StringBuilder resultSb = new StringBuilder();
+        for (byte aB : b) {
+            resultSb.append(byteToHexString(aB));
+        }
+        return resultSb.toString();
+    }
+
+    private String byteToHexString(byte b) {
+        int n = b;
+        if (n < 0) {
+            n = 256 + n;
+        }
+        int d1 = n / 16;
+        int d2 = n % 16;
+        return hexDigits[d1] + hexDigits[d2];
+    }
+
+    private String encodeMD5(String origin) {
+        String resultString = null;
+        try {
+            resultString = origin;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            resultString = byteArrayToHexString(md.digest(resultString.getBytes()));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return resultString;
+    }
+}
+
